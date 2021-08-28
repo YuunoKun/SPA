9f1cb1d5056d87e11014b158017e7cbe6c2ea888
Wincenttjoi <wincenttsjoi@gmail.com>
2021-08-29 00:13:07 +0800

    Add PKBAdapter.h file to initialize class

--- Team36/Code36/source/PKBAdapter.cpp
+++ Team36/Code36/source/PKBAdapter.cpp
@@ -1,1 +1,25 @@
 #include "PKBAdapter.h"
+#include "PKB.h"
+
+PKBAdapter::PKBAdapter() {
+}
+
+vector<variable_name> PKB::getVariables()
+{
+	return PKB::getVariables();
+}
+
+vector<Stmt> PKB::getStmts()
+{
+	return PKB::getStmts();
+}
+
+vector<constant> PKB::getConstants()
+{
+	return PKB::getConstants();
+}
+
+vector<procedure_name> PKB::getProcedures()
+{
+	return PKB::getProcedures();
+}
