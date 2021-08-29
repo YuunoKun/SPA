c8760efb5fa78455a8f4c0daaea7257d76a5a0ae
YangJiyu98 <yang.jiyu.jy@gmail.com>
2021-08-29 20:08:23 +0800

    add proc, constant, Select

--- Team36/Code36/source/QueryPreprocessor.cpp
+++ Team36/Code36/source/QueryPreprocessor.cpp
@@ -1,10 +1,5 @@
 #include "QueryPreprocessor.h"
-
-
-//Query QueryPreprocessor::process(string) {
-
-
-
+#include <iostream>
 
 
 
@@ -12,22 +7,76 @@
 //}
 
 Query QueryPreprocessor::process(std::string str) {
+	/*
+	* std::vector <std::string> tokens;
+	std::string delimiter = ";";
+	size_t pos = 0;
+	std::string token;
+	while ((pos = str.find(delimiter)) != std::string::npos) {
+		token = str.substr(0, pos);
+		tokens.push_back(token);
+		str.erase(0, pos + delimiter.length());
+	}
+	tokens.push_back(token);
+	*/
 
-	//std::string token = strtok((str.c_str()), ";");
+	const char delim = ';';
+	std::vector<std::string> tokens;
+	tokenize(str, delim, tokens);
+
 
 	// make new query
-	//Query query;
-
-	// assign declarations to entites
-	//add enetities 
-	
-	
-	// assign select to selected
-	//setSelected
+	Query query;
 
 
-	//return Query();
+	//check through all tokens
+	for (int i = 0; i < tokens.size(); i++) {
+
+		// EntityType: STMT | READ | PRINT | CALL | WHILE | IF | ASSIGN | VARIABLE | CONSTANT | PROCEDURE | wild?
+		// assign declarations to entites
+		// add enetities 
+		std::string CONSTANT = "constant";
+		std::string PROC = "proc";
+		std::string SELECT = "Select";
+
+		const char tempDelim = ' ';
+		std::vector<std::string> tempTokens;
+		tokenize(tokens[i], tempDelim, tempTokens);
+
+		if (tokens[i].find(CONSTANT) != std::string::npos) {
+			Entity entity(EntityType::CONSTANT, tempTokens[1]);
+			query.addEntity(entity);
+		}
+		else if (tokens[i].find(PROC) != std::string::npos) {
+			Entity entity(EntityType::PROCEDURE, tempTokens[1]);
+			query.addEntity(entity);
+		}
+		else if (tokens[i].find(SELECT) != std::string::npos) {
+			// need new type for SELECT?
+			Entity entity(EntityType::CONSTANT, tempTokens[1]);
+			query.setSelected(entity);
+		}
+		else {
+			//cout << "invalid query" << endl;
+		}
+
+		// assign select to selected
+		//setSelected
+		//== "Select"
+	}
+
+	return Query();
 }
 
+void tokenize(std::string const& str, const char delim,
+	std::vector<std::string>& out)
+{
+	size_t start;
+	size_t end = 0;
 
-// isValid(string) 
+	while ((start = str.find_first_not_of(delim, end)) != std::string::npos)
+	{
+		end = str.find(delim, start);
+		out.push_back(str.substr(start, end - start));
+	}
+}
