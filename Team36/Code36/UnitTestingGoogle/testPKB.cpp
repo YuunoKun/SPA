569835a4afd206f540f4a652dbe345d84fa8eb85
kelvinvin <kelvinw0ng@hotmail.com>
2021-08-29 17:07:00 +0800

    Add Google Test

---
+++ Team36/Code36/UnitTestingGoogle/testPKB.cpp
@@ -0,0 +1,23 @@
+#include "pch.h"
+
+#include "PKB.h"
+#include "PKB.cpp"
+#include "Common.h"
+
+TEST(PKB, getConstants) {
+	PKB pkb;
+	constant three = 3;
+	constant four = 4;
+	std::vector<constant> v{ three, four };
+	pkb.setConstants(v);
+	EXPECT_EQ(v, pkb.getConstants());
+}
+
+TEST(PKB, getProcedures) {
+	PKB pkb;
+	procedure_name first_procedure = "main";
+	procedure_name second_procedure = "printY";
+	std::vector<procedure_name> v{ first_procedure, second_procedure };
+	pkb.setProcedures(v);
+	EXPECT_EQ(v, pkb.getProcedures());
+}
