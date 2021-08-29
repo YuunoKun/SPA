573906ffb705d02dfac0c8ddccb45f21cc1eaeac
YangJiyu98 <yang.jiyu.jy@gmail.com>
2021-08-29 18:16:16 +0800

    add coments

--- Team36/Code36/source/QueryPreprocessor.cpp
+++ Team36/Code36/source/QueryPreprocessor.cpp
@@ -1,6 +1,33 @@
 #include "QueryPreprocessor.h"
 
 
-Query QueryPreprocessor::process(std::string) {
-	return Query();
+//Query QueryPreprocessor::process(string) {
+
+
+
+
+
+
+//	return Query();
+//}
+
+Query QueryPreprocessor::process(std::string str) {
+
+	//std::string token = strtok((str.c_str()), ";");
+
+	// make new query
+	//Query query;
+
+	// assign declarations to entites
+	//add enetities 
+	
+	
+	// assign select to selected
+	//setSelected
+
+
+	//return Query();
 }
+
+
+// isValid(string) 
