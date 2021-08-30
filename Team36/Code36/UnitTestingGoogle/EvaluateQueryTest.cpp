7e4c6d6b889415e7ec176e6a30df60b7ecbcaf19
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-08-30 22:24:43 +0800

    Add query evaluater unit test

---
+++ Team36/Code36/UnitTestingGoogle/EvaluateQueryTest.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+
+#include "QueryEvaluator.h"
+#include "Common.h"
+
+namespace UnitTesting {
+	TEST(QueryEvaluator, evaluateQueryVariable) {
+		Query q;
+		Entity e = { {},{} };
+		e.addValue("1");
+
+		QueryEvaluator qe;
+		qe.
+	}
+}
