4268e64863693fc4335b46d46c9642874ec20e95
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-09-01 07:07:29 +0800

    Fix query evalautor

--- Team36/Code36/Gui/GUIWrapper.cpp
+++ Team36/Code36/Gui/GUIWrapper.cpp
@@ -29,8 +29,10 @@ void GUIWrapper::evaluate(std::string query, std::list<std::string>& results) {
 	std::cout << "query=  " << query << std::endl;
 	QuerySystem querySystem;
 	results.push_back(query + "\nQuery result: ");
-	for each (std::string s in querySystem.processQuery(query)) {
+	std::list<std::string> queryResult = querySystem.processQuery(query);
+	for each (std::string s in queryResult) {
 		results.push_back(s);
+		std::cout << s << std::endl;
 	}
 	// store the answers to the query in the results list (it is initially empty)
 	// each result must be a string.
