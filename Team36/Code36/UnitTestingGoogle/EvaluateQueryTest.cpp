4e87d0948872c7eeb2fc380390caa258767be609
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-08-31 18:04:28 +0800

    Update query evalutor unit test to reset cach before each test

--- Team36/Code36/UnitTestingGoogle/EvaluateQueryTest.cpp
+++ Team36/Code36/UnitTestingGoogle/EvaluateQueryTest.cpp
@@ -8,6 +8,7 @@
 namespace UnitTesting {
 	TEST(QueryEvaluator, evaluateQueryVariable) {
 		//PKB::getInstance().setStmts({ {STMT_PRINT,1 }, {STMT_PRINT,2}, {STMT_PRINT,3} });
+		PKB::getInstance().resetCache();
 		PKB::getInstance().setVariables({ "a", "b", "c" });
 
 		std::list<std::string> result = { "a", "b", "c" };
@@ -22,6 +23,7 @@ namespace UnitTesting {
 	}
 
 	TEST(QueryEvaluator, evaluateQueryConstant) {
+		PKB::getInstance().resetCache();
 		PKB::getInstance().setConstants({ 1, 2, 3 });
 
 		std::list<std::string> result = { "1", "2", "3" };
@@ -36,6 +38,7 @@ namespace UnitTesting {
 	}
 
 	TEST(QueryEvaluator, evaluateQueryProcedures) {
+		PKB::getInstance().resetCache();
 		PKB::getInstance().setProcedures({ "x", "y", "z" });
 
 		std::list<std::string> result = { "x", "y", "z" };
