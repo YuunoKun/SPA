e9630f42e4b8385c352bc69a64a1d537b3d123d9
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-09-10 16:22:49 +0800

    Change Query Evaluator variables to const

--- Team36/Code36/UnitTestingGoogle/QueryEvaluatorTest.cpp
+++ Team36/Code36/UnitTestingGoogle/QueryEvaluatorTest.cpp
@@ -131,127 +131,127 @@ namespace UnitTesting {
 		}
 
 
-		var_name x = "x";
-		var_name y = "y";
-		var_name z = "z";
-		constant c1 = 1;
-		constant c2 = 2;
-		constant c3 = 3;
-		std::string c1s = std::to_string(c1);
-		std::string c2s = std::to_string(c2);
-		std::string c3s = std::to_string(c3);
-		proc_name p1 = "main";
-		proc_name p2 = "sub";
-		proc_name p3 = "sub1";
+		const var_name x = "x";
+		const var_name y = "y";
+		const var_name z = "z";
+		const constant c1 = 1;
+		const constant c2 = 2;
+		const constant c3 = 3;
+		const std::string c1s = std::to_string(c1);
+		const std::string c2s = std::to_string(c2);
+		const std::string c3s = std::to_string(c3);
+		const proc_name p1 = "main";
+		const proc_name p2 = "sub";
+		const proc_name p3 = "sub1";
 
-		std::string IF1 = "1";
-		std::string IF2 = "2";
-		std::string WHILE1 = "3";
-		std::string	WHILE2 = "4";
-		std::string READ1 = "5";
-		std::string READ2 = "6";
-		std::string PRINT1 = "7";
-		std::string PRINT2 = "8";
-		std::string ASSIGN1 = "9";
-		std::string ASSIGN2 = "10";
-		std::string CALL1 = "11";
-		std::string CALL2 = "12";
+		const std::string IF1 = "1";
+		const std::string IF2 = "2";
+		const std::string WHILE1 = "3";
+		const std::string	WHILE2 = "4";
+		const std::string READ1 = "5";
+		const std::string READ2 = "6";
+		const std::string PRINT1 = "7";
+		const std::string PRINT2 = "8";
+		const std::string ASSIGN1 = "9";
+		const std::string ASSIGN2 = "10";
+		const std::string CALL1 = "11";
+		const std::string CALL2 = "12";
 
-		std::string FOLLOW_LEFT1 = "1";
-		std::string FOLLOW_LEFT2 = "2";
-		std::string FOLLOW_RIGHT1 = "2";
-		std::string FOLLOW_RIGHT2 = "3";
+		const std::string FOLLOW_LEFT1 = "1";
+		const std::string FOLLOW_LEFT2 = "2";
+		const std::string FOLLOW_RIGHT1 = "2";
+		const std::string FOLLOW_RIGHT2 = "3";
 
-		std::vector<std::string> FOLLOW_LEFTS = { FOLLOW_LEFT1, FOLLOW_LEFT2 };
-		std::vector<std::string> FOLLOW_RIGHTS = { FOLLOW_RIGHT1, FOLLOW_RIGHT2 };
+		const std::vector<std::string> FOLLOW_LEFTS = { FOLLOW_LEFT1, FOLLOW_LEFT2 };
+		const std::vector<std::string> FOLLOW_RIGHTS = { FOLLOW_RIGHT1, FOLLOW_RIGHT2 };
 
-		std::string PARENT_LEFT1 = "1";
-		std::string PARENT_LEFT2 = "2";
-		std::string PARENT_LEFT3 = "3";
-		std::string PARENT_RIGHT1 = "2";
-		std::string PARENT_RIGHT2 = "3";
-		std::string PARENT_RIGHT3 = "4";
+		const std::string PARENT_LEFT1 = "1";
+		const std::string PARENT_LEFT2 = "2";
+		const std::string PARENT_LEFT3 = "3";
+		const std::string PARENT_RIGHT1 = "2";
+		const std::string PARENT_RIGHT2 = "3";
+		const std::string PARENT_RIGHT3 = "4";
 
-		std::vector<std::string> PARENT_LEFTS = { PARENT_LEFT1, PARENT_LEFT2, PARENT_LEFT3 };
-		std::vector<std::string> PARENT_RIGHTS = { PARENT_RIGHT1, PARENT_RIGHT2, PARENT_RIGHT3 };
+		const std::vector<std::string> PARENT_LEFTS = { PARENT_LEFT1, PARENT_LEFT2, PARENT_LEFT3 };
+		const std::vector<std::string> PARENT_RIGHTS = { PARENT_RIGHT1, PARENT_RIGHT2, PARENT_RIGHT3 };
 
-		std::string MODIFIES_LEFT1 = "1";
-		std::string MODIFIES_LEFT2 = "3";
-		std::string MODIFIES_RIGHT1 = x;
-		std::string MODIFIES_RIGHT2 = y;
+		const std::string MODIFIES_LEFT1 = "1";
+		const std::string MODIFIES_LEFT2 = "3";
+		const std::string MODIFIES_RIGHT1 = x;
+		const std::string MODIFIES_RIGHT2 = y;
 
-		std::string MODIFIES_LEFT3 = ASSIGN1;
-		std::string MODIFIES_LEFT4 = ASSIGN2;
-		std::string MODIFIES_RIGHT3 = x;
-		std::string MODIFIES_RIGHT4 = y;
+		const std::string MODIFIES_LEFT3 = ASSIGN1;
+		const std::string MODIFIES_LEFT4 = ASSIGN2;
+		const std::string MODIFIES_RIGHT3 = x;
+		const std::string MODIFIES_RIGHT4 = y;
 
-		std::string EXPRESSION1 = "x";
-		std::string EXPRESSION2= "x + y";
+		const std::string EXPRESSION1 = "x";
+		const std::string EXPRESSION2= "x + y";
 
-		std::vector<std::string> MODIFIES_LEFTS = { MODIFIES_LEFT1, MODIFIES_LEFT2, MODIFIES_LEFT3, MODIFIES_LEFT4 };
-		std::vector<std::string> MODIFIES_RIGHTS = { MODIFIES_RIGHT1, MODIFIES_RIGHT2, MODIFIES_RIGHT3, MODIFIES_RIGHT4 };
-		std::vector<std::string> EXPRESSIONS = { EXPRESSION1, EXPRESSION2 };
+		const std::vector<std::string> MODIFIES_LEFTS = { MODIFIES_LEFT1, MODIFIES_LEFT2, MODIFIES_LEFT3, MODIFIES_LEFT4 };
+		const std::vector<std::string> MODIFIES_RIGHTS = { MODIFIES_RIGHT1, MODIFIES_RIGHT2, MODIFIES_RIGHT3, MODIFIES_RIGHT4 };
+		const std::vector<std::string> EXPRESSIONS = { EXPRESSION1, EXPRESSION2 };
 
-		std::string USES_LEFT1 = "1";
-		std::string USES_LEFT2 = "3";
-		std::string USES_RIGHT1 = y;
-		std::string USES_RIGHT2 = x;
+		const std::string USES_LEFT1 = "1";
+		const std::string USES_LEFT2 = "3";
+		const std::string USES_RIGHT1 = y;
+		const std::string USES_RIGHT2 = x;
 
-		std::vector<std::string> USES_LEFTS = { USES_LEFT1, USES_LEFT2 };
-		std::vector<std::string> USES_RIGHTS = { USES_RIGHT1, USES_RIGHT2 };
+		const std::vector<std::string> USES_LEFTS = { USES_LEFT1, USES_LEFT2 };
+		const std::vector<std::string> USES_RIGHTS = { USES_RIGHT1, USES_RIGHT2 };
 
-		std::list<std::string> stmts = { IF1, IF2, WHILE1, WHILE2, READ1, READ2,
+		const std::list<std::string> STMTS = { IF1, IF2, WHILE1, WHILE2, READ1, READ2,
 			PRINT1, PRINT2, ASSIGN1, ASSIGN2, CALL1, CALL2 };
 		PKBAdapter pkb;
 		QueryEvaluator evaluator;
 
-		Synonym COMMON_SYNONYM1 = { "cs1" };
-		Synonym COMMON_SYNONYM2 = { "cs2" };
+		const Synonym COMMON_SYNONYM1 = { "cs1" };
+		const Synonym COMMON_SYNONYM2 = { "cs2" };
 
 		// select v
-		std::list<std::string> ALL_VARIABLE = { x, y, z };
-		Entity selectVariable = { VARIABLE, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_VARIABLE = { x, y, z };
+		const Entity SELECT_VARIABLE = { VARIABLE, COMMON_SYNONYM1 };
 		// select c
-		std::list<std::string> ALL_CONSTANT = { c1s, c2s, c3s };
-		Entity selectConstant = { CONSTANT, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_CONSTANT = { c1s, c2s, c3s };
+		const Entity SELECT_CONSTANT = { CONSTANT, COMMON_SYNONYM1 };
 		// select p
-		std::list<std::string> ALL_PROCEDURE = { p1, p2, p3 };
-		Entity selectProcedure = { PROCEDURE, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_PROCEDURE = { p1, p2, p3 };
+		const Entity SELECT_PROCEDURE = { PROCEDURE, COMMON_SYNONYM1 };
 		// select s
-		std::list<std::string> ALL_STMT = stmts;
-		Entity selectStmt = { STMT, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_STMT = STMTS;
+		const Entity SELECT_STMT = { STMT, COMMON_SYNONYM1 };
 		// select ifs
-		std::list<std::string> ALL_IF = { IF1, IF2 };
-		Entity selectIfs = { IF, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_IF = { IF1, IF2 };
+		const Entity SELECT_IF = { IF, COMMON_SYNONYM1 };
 		// select w
-		std::list<std::string> ALL_WHILE = { WHILE1, WHILE2 };
-		Entity selectWhile = { WHILE, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_WHILE = { WHILE1, WHILE2 };
+		const Entity SELECT_WHILE = { WHILE, COMMON_SYNONYM1 };
 		// select read
-		std::list<std::string> ALL_READ = { READ1, READ2 };
-		Entity selectRead = { READ, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_READ = { READ1, READ2 };
+		const Entity SELECT_READ = { READ, COMMON_SYNONYM1 };
 		// select print
-		std::list<std::string> ALL_PRINT = { PRINT1, PRINT2 };
-		Entity selectPrint = { PRINT, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_PRINT = { PRINT1, PRINT2 };
+		const Entity SELECT_PRINT = { PRINT, COMMON_SYNONYM1 };
 		// select assign
-		std::list<std::string> ALL_ASSIGN = { ASSIGN1, ASSIGN2 };
-		Entity selectAssign = { ASSIGN, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_ASSIGN = { ASSIGN1, ASSIGN2 };
+		const Entity SELECT_ASSIGN = { ASSIGN, COMMON_SYNONYM1 };
 		// select call
-		std::list<std::string> ALL_CALL = { CALL1, CALL2 };
-		Entity selectCall = { CALL, COMMON_SYNONYM1 };
+		const std::list<std::string> ALL_CALL = { CALL1, CALL2 };
+		const Entity SELECT_CALL = { CALL, COMMON_SYNONYM1 };
 
-		std::vector<std::list<std::string>> ALL_RESULT = {
+		const std::vector<std::list<std::string>> ALL_RESULT = {
 			ALL_VARIABLE , ALL_CONSTANT , ALL_PROCEDURE, ALL_STMT, ALL_IF,
 			ALL_WHILE, ALL_READ, ALL_PRINT, ALL_ASSIGN, ALL_CALL };
 
-		std::vector<Entity> ALL_SELECT = {
-			selectVariable , selectConstant , selectProcedure, selectStmt, selectIfs,
-			selectWhile, selectRead, selectPrint, selectAssign, selectCall };
+		const std::vector<Entity> ALL_SELECT = {
+			SELECT_VARIABLE , SELECT_CONSTANT , SELECT_PROCEDURE, SELECT_STMT, SELECT_IF,
+			SELECT_WHILE, SELECT_READ, SELECT_PRINT, SELECT_ASSIGN, SELECT_CALL };
 
-		std::list<std::string> EMPTY_RESULT = {};
+		const std::list<std::string> EMPTY_RESULT = {};
 
-		Entity WILD_CARD = { WILD };
+		const Entity WILD_CARD = { WILD };
 
-		std::vector<Entity> VALID_CONSTANT_STMT_ENTITY = { WILD_CARD,
+		const std::vector<Entity> VALID_CONSTANT_STMT_ENTITY = { WILD_CARD,
 			{STMT, "1"}, {STMT, "2"}, {STMT, "3"}, {STMT, "4"}, {STMT, "5"}, {STMT, "6"},
 			{STMT, "7"}, {STMT, "8"}, {STMT, "9"}, {STMT, "10"}, {STMT, "11"}, {STMT, "12"}
 		};
@@ -264,7 +264,8 @@ namespace UnitTesting {
 	TEST_F(QueryEvaluatorTest, evaluateQueryRaw) {
 		for (unsigned int i = 0; i < ALL_SELECT.size(); i++) {
 			Query q;
-			q.addSelected(ALL_SELECT[i]);
+			Entity selected = ALL_SELECT[i];
+			q.addSelected(selected);
 			EXPECT_EQ(evaluator.evaluateQuery(q), ALL_RESULT[i]) << "Error at results : " << i + 1;
 		}
 	}
@@ -1914,6 +1915,11 @@ namespace UnitTesting {
 		std::vector<Pattern> patterns;
 		std::vector<std::list<std::string>> results;
 		std::vector<Entity> selected;
+
+		//Handle result for Select a pattern a(_, _)
+		patterns.push_back(Pattern(assignCommon, WILD_CARD, "", true));
+		selected.push_back(assignCommon);
+		results.push_back({ left1, left2 });
 
 		//Handle result for Select a pattern a(v, _)
 		patterns.push_back(Pattern(assignCommon, lhsCommon, "", true));
