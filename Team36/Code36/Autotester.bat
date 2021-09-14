dd91c1f738364f51354fc021160bb3f5b287277b
kelvinvin <kelvinw0ng@hotmail.com>
2021-09-15 00:57:10 +0800

    Add Autotester.bat

---
+++ Team36/Code36/Autotester.bat
@@ -0,0 +1,15 @@
+@echo off
+title Iteration 1 queries
+
+Debug\AutoTester.exe ..\Tests36\Sample_source.txt ..\Tests36\Sample_queries.txt .\Tests\out-sample.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_1.txt ..\Tests36\Query_test_1.txt .\Tests\out-1.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_2.txt ..\Tests36\Query_test_2.txt .\Tests\out-2.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_3.txt ..\Tests36\Query_test_3.txt .\Tests\out-3.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_4.txt ..\Tests36\Query_test_4.txt .\Tests\out-4.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_5.txt ..\Tests36\Query_test_5.txt .\Tests\out-5.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_6.txt ..\Tests36\Query_test_6.txt .\Tests\out-6.xml
+Debug\AutoTester.exe ..\Tests36\Source_test_6.txt ..\Tests36\Query_test_7.txt .\Tests\out-7.xml
+
+echo FINISHED
+pause
+exit
