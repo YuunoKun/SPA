8d2f08360a6fe0aced55df983ddc95213ac1b1e6
Wincenttjoi <wincenttsjoi@gmail.com>
2021-09-14 23:59:32 +0800

    Add unit test for declaration and select, positive

--- Team36/Code36/UnitTestingGoogle/QueryPatternSuchThatValidationTest.cpp
+++ Team36/Code36/UnitTestingGoogle/QueryPatternSuchThatValidationTest.cpp
@@ -5,8 +5,6 @@
 #include "Common.h"
 #include <iostream>
 
-
-
 namespace UnitTesting {
 	class QueryPatternSuchThatValidationTest : public ::testing::Test {
 	protected:
@@ -28,11 +26,10 @@ namespace UnitTesting {
 
 		// Class members declared here can be used by all tests in the test suite
 		// for Foo.
-
 	};
 
 	// Test setStmtRef
-	// 3 diff params 
+	// 3 diff params
 	TEST(QueryPreprocessor, setStmtRefTest) {
 		QueryPreprocessor qpp;
 
@@ -87,7 +84,7 @@ namespace UnitTesting {
 	//	synonym.name = "s";
 	//	Entity expected_declared_stmt = Entity(EntityType::STMT, synonym);
 	//	query.addEntity(expected_declared_stmt);
-	//	
+	//
 	//	//Result
 	//	QueryToken stmt_s_token = QueryToken(QueryToken::IDENTIFIER, "s");
 	//	temp_token_chain.push_back(stmt_s_token);
@@ -166,7 +163,7 @@ namespace UnitTesting {
 
 	//	//Result
 	//	QueryPreprocessor::parseParameterSuchThat(query, QueryToken::MODIFIES_S, temp_token_chain);
-	//	
+	//
 	//	EXPECT_TRUE(query.getRelations()[0] == expected_rel);
 	//}
 
@@ -210,8 +207,6 @@ namespace UnitTesting {
 	//	temp_token_chain.push_back({ QueryToken::IDENTIFIER, "s" });
 	//	temp_token_chain.push_back({ QueryToken::QUOTATION_CLOSE, "" });
 
-
-
 	//	std::vector<QueryToken> left_expr_token_chain;
 	//	left_expr_token_chain.push_back({ QueryToken::QUOTATION_OPEN, "" });
 	//	left_expr_token_chain.push_back({ QueryToken::IDENTIFIER, "procName" });
@@ -219,7 +214,6 @@ namespace UnitTesting {
 
 	//	std::vector<QueryToken> right_expr_token_chain;
 	//	right_expr_token_chain.push_back({ QueryToken::IDENTIFIER, "s" });
-
 
 	//	//Synonym
 	//	//Expected
@@ -231,7 +225,7 @@ namespace UnitTesting {
 	//	Entity expected_ent_wildcard = Entity(EntityType::WILD);
 
 	//	Pattern expected_pat = Pattern(expected_declared_assign, setEntRef(query, left_expr_token_chain), setExpr(right_expr_token_chain), false);
-	//	
+	//
 	//	//Result
 	//	parseParameterPattern(query, expected_declared_assign, temp_token_chain);
 
@@ -253,9 +247,6 @@ namespace UnitTesting {
 	//	temp_token_chain.push_back({ QueryToken::QUOTATION_CLOSE, "" });
 	//	temp_token_chain.push_back({ QueryToken::WILDCARD, "" });
 
-
-
-
 	//	std::vector<QueryToken> left_expr_token_chain;
 	//	left_expr_token_chain.push_back({ QueryToken::QUOTATION_OPEN, "" });
 	//	left_expr_token_chain.push_back({ QueryToken::IDENTIFIER, "procName" });
@@ -263,7 +254,6 @@ namespace UnitTesting {
 
 	//	std::vector<QueryToken> right_expr_token_chain;
 	//	right_expr_token_chain.push_back({ QueryToken::IDENTIFIER, "s" });
-
 
 	//	//Synonym
 	//	//Expected
@@ -293,16 +283,12 @@ namespace UnitTesting {
 	//	temp_token_chain.push_back({ QueryToken::COMMA, "" });
 	//	temp_token_chain.push_back({ QueryToken::WILDCARD, "" });
 
-
-
-
 	//	std::vector<QueryToken> left_expr_token_chain;
 	//	left_expr_token_chain.push_back({ QueryToken::QUOTATION_OPEN, "" });
 	//	left_expr_token_chain.push_back({ QueryToken::IDENTIFIER, "procName" });
 	//	left_expr_token_chain.push_back({ QueryToken::QUOTATION_CLOSE, "" });
 
 	//	std::vector<QueryToken> right_expr_token_chain;
-
 
 	//	//Synonym
 	//	//Expected
@@ -321,4 +307,345 @@ namespace UnitTesting {
 	//	EXPECT_TRUE(query.getPatterns()[0] == expected_pat);
 	//}
 
+	TEST(parse, addSingleStatement) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("stmt s;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::STMT, Synonym{ "s" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::STMT, Synonym{ "s1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleStatements) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("stmt s, s1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::STMT, Synonym{ "s" }));
+		q.addEntity(Entity(EntityType::STMT, Synonym{ "s1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::STMT, Synonym{ "s2" }));
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleProcedure) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("procedure p;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::PROCEDURE, Synonym{ "p" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::STMT, Synonym{ "p1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleProcedures) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("procedure p, p1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::PROCEDURE, Synonym{ "p" }));
+		q.addEntity(Entity(EntityType::PROCEDURE, Synonym{ "p1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::PROCEDURE, Synonym{ "p2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleRead) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("read r;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::READ, Synonym{ "r" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::READ, Synonym{ "r1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleReads) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("read r, r1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::READ, Synonym{ "r" }));
+		q.addEntity(Entity(EntityType::READ, Synonym{ "r1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::READ, Synonym{ "r2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSinglePrint) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("print pr;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::PRINT, Synonym{ "pr" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::PRINT, Synonym{ "pr1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultiplePrints) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("print pr, pr1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::PRINT, Synonym{ "pr" }));
+		q.addEntity(Entity(EntityType::PRINT, Synonym{ "pr1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::PRINT, Synonym{ "pr2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleCall) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("call c;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::CALL, Synonym{ "c" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::CALL, Synonym{ "c1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleCalls) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("call c, c1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::CALL, Synonym{ "c" }));
+		q.addEntity(Entity(EntityType::CALL, Synonym{ "c1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::CALL, Synonym{ "c2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleWhile) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("while w;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::WHILE, Synonym{ "w" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::WHILE, Synonym{ "w1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleWhiles) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("while w, w1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::WHILE, Synonym{ "w" }));
+		q.addEntity(Entity(EntityType::WHILE, Synonym{ "w1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::WHILE, Synonym{ "w2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleIf) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("ifs if;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::IF, Synonym{ "if" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::IF, Synonym{ "if1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleIfs) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("ifs if, if1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::IF, Synonym{ "if" }));
+		q.addEntity(Entity(EntityType::IF, Synonym{ "if1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::IF, Synonym{ "if2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleAssign) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("assign a;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::ASSIGN, Synonym{ "a" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::ASSIGN, Synonym{ "a1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleAssigns) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("assign a, a1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::ASSIGN, Synonym{ "a" }));
+		q.addEntity(Entity(EntityType::ASSIGN, Synonym{ "a1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::ASSIGN, Synonym{ "a2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleVariable) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("variable v;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "v" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "v1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleVariables) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("variable v, v1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "v" }));
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "v1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "v2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addSingleConstant) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("constant c;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::CONSTANT, Synonym{ "c" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "c1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleConstants) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("constant c, c1;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::CONSTANT, Synonym{ "c" }));
+		q.addEntity(Entity(EntityType::CONSTANT, Synonym{ "c1" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::CONSTANT, Synonym{ "c2" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addMultipleDeclarations) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("variable v; procedure p;");
+
+		Query q;
+		q.addEntity(Entity(EntityType::VARIABLE, Synonym{ "v" }));
+		q.addEntity(Entity(EntityType::PROCEDURE, Synonym{ "p" }));
+
+		EXPECT_EQ(test.getEntities(), q.getEntities());
+
+		q.addEntity(Entity(EntityType::PROCEDURE, Synonym{ "p1" }));
+
+		EXPECT_NE(test.getEntities(), q.getEntities());
+	}
+
+	TEST(parse, addValidSelect) {
+		QueryPreprocessor qp;
+		Query test = qp.parse("procedure p; Select p");
+
+		Query q;
+		q.addSelected(Entity(EntityType::PROCEDURE, Synonym{ "p" }));
+
+		EXPECT_EQ(test.getSelected(), q.getSelected());
+	}
+
+	TEST(parse, validSuchthatUses) {
+		//TODO
+	}
+
+	TEST(parse, validSuchthatModifies) {
+		//TODO
+	}
+
+	TEST(parse, validSuchthatParent) {
+		//TODO
+	}
+
+	TEST(parse, validSuchthatParentT) {
+		//TODO
+	}
+
+	TEST(parse, validSuchthatFollows) {
+		//TODO
+	}
+
+	TEST(parse, validSuchthatFollowsT) {
+		//TODO
+	}
+
+	TEST(parse, validPattern) {
+		//TODO
+	}
 }
