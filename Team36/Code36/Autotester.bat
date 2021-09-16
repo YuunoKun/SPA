10c1f7200ad3cd61e6b1da6d5dc84e66d5ca7730
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-09-16 21:06:24 +0800

    Add multi_clauses file

--- Team36/Code36/Autotester.bat
+++ Team36/Code36/Autotester.bat
@@ -10,6 +10,7 @@ Debug\AutoTester.exe ..\Tests36\Source_test_5.txt ..\Tests36\Query_test_5.txt .\
 Debug\AutoTester.exe ..\Tests36\Source_test_6.txt ..\Tests36\Query_test_6.txt .\Tests\out-6.xml
 Debug\AutoTester.exe ..\Tests36\Source_test_7.txt ..\Tests36\Query_test_7.txt .\Tests\out-7.xml
 Debug\AutoTester.exe ..\Tests36\container_constants_source.txt ..\Tests36\container_constants_queries.txt .\Tests\out-container_constants.xml
+Debug\AutoTester.exe ..\Tests36\multi_clauses_source.txt ..\Tests36\multi_clauses_queries.txt .\Tests\out-multi-clauses.xml
 
 echo FINISHED
 pause
