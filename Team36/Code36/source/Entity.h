2fec1fddbe1d71c52d31bd5949b6818302d4e100
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-10-01 20:10:18 +0800

    Add attribute to Entity

--- Team36/Code36/source/Entity.h
+++ Team36/Code36/source/Entity.h
@@ -18,17 +18,27 @@ enum EntityType {
 	WILD
 };
 
+enum AttrRef {
+	STMT_INDEX,
+	PROC_NAME,
+	VAR_NAME,
+	VALUE,
+	NONE
+};
+
 class Entity {
 public:
 	Entity();
 
 	Entity(EntityType);
 	Entity(EntityType, Synonym);
+	Entity(EntityType, Synonym, AttrRef);
 	Entity(EntityType, std::string);
 
 	EntityType getType();
 	std::string getSynonym();
 	std::string getValue();
+	AttrRef getAttribute();
 	bool isSynonym();
 
 	bool operator==(const Entity&) const;
@@ -39,5 +49,8 @@ private:
 	EntityType entity_type;
 	Synonym synonym;
 	std::string value;
+	AttrRef attribute;
 	bool is_synonym;
+
+	void defaultAttribute(EntityType);
 };
