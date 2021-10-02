9b2173f03def36bbecb4de7363e156952383b539
kelvinvin <kelvinw0ng@hotmail.com>
2021-10-02 22:08:19 +0800

    Refactor ParserPKB integration test bonus

--- Team36/Code36/IntegrationTestingGoogle/ParserPKBTest.cpp
+++ Team36/Code36/IntegrationTestingGoogle/ParserPKBTest.cpp
@@ -57,6 +57,30 @@ namespace IntegrationTesting {
 		EXPECT_EQ(PKB::getInstance().getConstants().size(), 4);
 	}
 
+	TEST_F(ParserPKBTest, DuplicateProcedureNames) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/duplicate_procedure_name_source.txt");
+		ASSERT_THROW(parser.parse(), std::runtime_error);
+	}
+
+	TEST_F(ParserPKBTest, CallUndefinedProcedureName) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/call_undefined_procedure_source.txt");
+		ASSERT_THROW(parser.parse(), std::runtime_error);
+	}
+
+	TEST_F(ParserPKBTest, RecursiveCallSource) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/recursive_calls_source.txt");
+		ASSERT_THROW(parser.parse(), std::runtime_error);
+	}
+
+	TEST_F(ParserPKBTest, CyclicCallSource) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/cyclic_calls_source.txt");
+		ASSERT_THROW(parser.parse(), std::runtime_error);
+	}
+
 	TEST_F(ParserPKBTest, Sample5Test_Entities) {
 		SourceProcessor::Parser parser;
 		parser.load_file("../UnitTestingGoogle/SPTest/TestSource/Sample5.txt");
@@ -232,5 +256,76 @@ namespace IntegrationTesting {
 		auto v = table.getPairs();
 		std::sort(v.begin(), v.end());
 		EXPECT_EQ(v, expected_modifiesS_stmt);
+	}
+
+	TEST_F(ParserPKBTest, NoProcedureCallsTest_UsesP) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/no_procedure_call_source.txt");
+		parser.parse();
+
+		std::vector<std::pair<proc_name, var_name>> expected_usesP = {
+			{"mySecondProcedure", "x"}, {"mySecondProcedure", "y"}
+		};
+		std::sort(expected_usesP.begin(), expected_usesP.end());
+
+		RelationTable<proc_name, var_name> table = PKB::getInstance().getUsesP();
+		auto v = table.getPairs();
+		std::sort(v.begin(), v.end());
+		EXPECT_EQ(v, expected_usesP);
+	}
+
+	TEST_F(ParserPKBTest, NoProcedureCallsTest_ModifiesP) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/no_procedure_call_source.txt");
+		parser.parse();
+
+		std::vector<std::pair<proc_name, var_name>> expected_modifiesP = {
+			{"myFirstProcedure", "x"}, {"myFirstProcedure", "y"},
+			{"mySecondProcedure", "z"}, {"mySecondProcedure", "x"}, {"mySecondProcedure", "w"}
+		};
+		std::sort(expected_modifiesP.begin(), expected_modifiesP.end());
+
+		RelationTable<proc_name, var_name> table = PKB::getInstance().getModifiesP();
+		auto v = table.getPairs();
+		std::sort(v.begin(), v.end());
+		EXPECT_EQ(v, expected_modifiesP);
+	}
+
+	TEST_F(ParserPKBTest, ProcedureCallsTest_UsesP) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/procedure_call_source.txt");
+		parser.parse();
+
+		std::vector<std::pair<proc_name, var_name>> expected_usesP = {
+			{"main", "x"}, {"main", "y"},{"main", "count"},{"main", "cenX"},{"main", "cenY"},
+			{"printResults", "cenX"},{"printResults", "cenY"},
+			{"computeCentroid", "x"}, {"computeCentroid", "y"},{"computeCentroid", "count"},
+				{"computeCentroid", "cenX"},{"computeCentroid", "cenY"}
+		};
+		std::sort(expected_usesP.begin(), expected_usesP.end());
+
+		RelationTable<proc_name, var_name> table = PKB::getInstance().getUsesP();
+		auto v = table.getPairs();
+		std::sort(v.begin(), v.end());
+		EXPECT_EQ(v, expected_usesP);
+	}
+
+	TEST_F(ParserPKBTest, ProcedureCallsTest_ModifiesP) {
+		SourceProcessor::Parser parser;
+		parser.load_file("../IntegrationTestingGoogle/Tests/procedure_call_source.txt");
+		parser.parse();
+
+		std::vector<std::pair<proc_name, var_name>> expected_modifiesP = {
+			{"main", "count"}, {"main", "x"}, {"main", "flag"},{"main", "cenX"},{"main", "normSq"},
+			{"readPoint", "x"},
+			{"computeCentroid", "count"}, {"computeCentroid", "x"},{"computeCentroid", "flag"},
+				{"computeCentroid", "cenX"},{"computeCentroid", "normSq"}
+		};
+		std::sort(expected_modifiesP.begin(), expected_modifiesP.end());
+
+		RelationTable<proc_name, var_name> table = PKB::getInstance().getModifiesP();
+		auto v = table.getPairs();
+		std::sort(v.begin(), v.end());
+		EXPECT_EQ(v, expected_modifiesP);
 	}
 }
