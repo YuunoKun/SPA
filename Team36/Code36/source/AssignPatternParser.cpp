40a1211f60f6681f04898363ceb46fda58f293e1
YangJiyu98 <yang.jiyu.jy@gmail.com>
2021-10-09 15:23:14 +0800

    update assign

--- Team36/Code36/source/AssignPatternParser.cpp
+++ Team36/Code36/source/AssignPatternParser.cpp
@@ -12,22 +12,23 @@ void AssignPatternParser::parseAssign(Query& query, Entity& entity, std::vector<
     QueryPatternRelRefParser parser;
     std::vector<QueryToken> temp_token_chain_1;
     std::vector<QueryToken> temp_token_chain_2;
-    bool comma_found = false;
+    bool comma_count = 0;
     bool is_wild = false;
     size_t token_chain_size = token_chain.size();
     for (size_t i = 0; i < token_chain_size; i++) {
         if (token_chain[0].type == QueryToken::COMMA) {
             token_chain.erase(token_chain.begin());
-            comma_found = true;
-        } else if (!comma_found) {
+            comma_count += 1;
+        } else if (comma_count == 0) {
             // 1st param
             temp_token_chain_1.push_back(token_chain[0]);
             token_chain.erase(token_chain.begin());
-        }
-        else {
+        } else if (comma_count == 1){
             // 2nd param
             temp_token_chain_2.push_back(token_chain[0]);
             token_chain.erase(token_chain.begin());
+        } else {
+            throw SyntacticErrorException("Invalid parameters for assign pattern");
         }
     }
 
