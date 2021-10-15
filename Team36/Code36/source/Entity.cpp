44b783883b89f5a66639ebcaefe8eb6fe9b51621
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-10-16 03:39:45 +0800

    Merge conflict

--- Team36/Code36/source/Entity.cpp
+++ Team36/Code36/source/Entity.cpp
@@ -67,7 +67,6 @@ void Entity::defaultAttribute(EntityType type) {
 	case EntityType::READ:
 	case EntityType::PRINT:
 	case EntityType::CALL:
-	case EntityType::PROG_LINE:
 	case EntityType::WHILE:
 	case EntityType::IF:
 	case EntityType::ASSIGN: attribute = AttrRef::STMT_INDEX;
