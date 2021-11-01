98cb7036359eefca81f2c058ca550a43116f8c7f
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-11-02 02:03:22 +0800

    Add query evaluator join table stress test

---
+++ Team36/Code36/UnitTestingGoogle/QueryEvaluatorStressTest.cpp
@@ -0,0 +1,103 @@
+#include "pch.h"
+
+#include <chrono>
+
+#include "QueryEvaluator.h"
+#include "PKBAdapter.h"
+#include "PKB.h"
+#include "Common.h"
+#include "ExprParser.h"
+#include "Utility.h"
+
+namespace UnitTesting {
+	class QueryEvaluatorStressTest : public testing::Test {
+	protected:
+		QueryEvaluatorStressTest() {
+			for (int i = 0; i < source_code_size.size(); i++) {
+				tables.push_back(generateTable(source_code_size[i]));
+				tables2.push_back(generateTable(source_code_size[i]));
+				join_tables.push_back(generateJoinTable(source_code_size[i]));
+			}
+		}
+
+		std::list<vector<std::string>> generateTable(int size) {
+			std::list<vector<std::string>> table;
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					table.push_back({ std::to_string(i), std::to_string(j) });
+				}
+			}
+			return table;
+		}
+		
+		std::unordered_multimap<std::string, std::vector<std::string>> generateJoinTable(int size) {
+			std::unordered_multimap<std::string, std::vector<std::string>> table;
+
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					table.insert({ to_string(i), { std::to_string(i), std::to_string(j) } });
+				}
+			}
+			return table;
+		}
+		void startTimer() {
+			startTime = std::chrono::high_resolution_clock::now();
+		}
+		void endTimer() {
+			endTime = std::chrono::high_resolution_clock::now();
+		}
+		void printTime(std::string message) {
+			// Call the function, here sort()
+			auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime);
+
+			std::cout << message << duration.count() << " milliseconds" << endl;
+		}
+
+		std::chrono::steady_clock::time_point startTime, endTime;
+
+		bool print_result = true;
+		// table size = x ^ 2
+		vector<int> source_code_size = { 10, 20, 30, 40, 50, 100 };//, 250 };
+		vector<std::list<vector<std::string>>> tables;
+		vector<std::list<vector<std::string>>> tables2;
+		vector<std::unordered_multimap<std::string, std::vector<std::string>>> join_tables;
+	};
+
+	// Join Table merge column algorithm Speed Test
+	TEST_F(QueryEvaluatorStressTest, join_table_common_column) {
+		for (int i = 0; i < source_code_size.size(); i++) {
+			startTimer();
+			Utility::joinTable(tables[i], 0, join_tables[i], 0);
+			endTimer();
+			printTime("Codes of size " + to_string(source_code_size[i]) + " takes : ");
+		}
+
+		std::cout << " end" << endl;
+		EXPECT_TRUE(!print_result);
+	}
+
+	// Join Table algorithm Speed Test 
+	TEST_F(QueryEvaluatorStressTest, join_table) {
+		std::cout << "list max size: " << tables.max_size() << endl;
+		std::cout << "vector max size: " << source_code_size.max_size() << endl;
+		for (int i = 0; i < source_code_size.size(); i++) {
+			startTimer();
+			Utility::joinTable(tables[i], tables2[i]);
+			endTimer();
+			printTime("Codes of size " + to_string(source_code_size[i]) + " takes : ");
+		}
+		std::cout << " end" << endl;
+		EXPECT_TRUE(!print_result);
+	}
+
+	TEST_F(QueryEvaluatorStressTest, filter_table) {
+		for (int i = 0; i < source_code_size.size(); i++) {
+			startTimer();
+			Utility::filterResults(tables[i],0, 1, tables2[i],0,1);
+			endTimer();
+			printTime("Codes of size " + to_string(source_code_size[i]) + " takes : ");
+		}
+		std::cout << " end" << endl;
+		EXPECT_TRUE(!print_result);
+	}
+}
