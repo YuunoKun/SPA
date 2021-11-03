ea796bc905af7fcaa5015edcf28199fd1bb8cbfc
TeoHoeKeat <TeoHoeKeat@gmail.com>
2021-11-03 21:33:52 +0800

    Update stress test

--- Team36/Code36/AutotesterStress.bat
+++ Team36/Code36/AutotesterStress.bat
@@ -4,6 +4,9 @@ title Stress Test queries
 Release\AutoTester.exe ..\Tests36\stress_test_50_source.txt ..\Tests36\stress_test_queries.txt .\Tests\stress-test-1-query.xml
 Release\AutoTester.exe ..\Tests36\stress_test_250_source.txt ..\Tests36\stress_test_queries.txt .\Tests\stress-test-2-query.xml
 Release\AutoTester.exe ..\Tests36\stress_test_500_source.txt ..\Tests36\stress_test_queries.txt .\Tests\stress-test-3-query.xml
+Release\AutoTester.exe ..\Tests36\stress_test_extreme_50_source.txt ..\Tests36\stress_test_queries.txt .\Tests\stress-test-extreme-1-query.xml
+Release\AutoTester.exe ..\Tests36\stress_test_extreme_250_source.txt ..\Tests36\stress_test_queries.txt .\Tests\stress-test-extreme-2-query.xml
+Release\AutoTester.exe ..\Tests36\stress_test_extreme_500_source.txt ..\Tests36\stress_test_queries.txt .\Tests\stress-test-extreme-3-query.xml
 
 echo FINISHED
 pause
